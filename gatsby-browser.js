a31a2b88618a85642bca7be3a69cb0ea078017d8
Che To <chekinto2@hotmail.com>
2021-04-06 08:12:07 +0100

    trying to fix layout bug

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { GlobalStyles } from './src/styles/global'
-import { Layout } from './src/components'
+import { Layout } from './src/components/Layout'
 import { AppContextProvider } from './src/context'
 
 export const wrapPageElement = ({ element, props }) => {
